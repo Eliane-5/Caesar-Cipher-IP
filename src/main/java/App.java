7d3a77ba65d167df6596413e805c17a83dfdaa9d
Eliane-5 <irankundaeliane9@gmail.com>
2019-09-09 16:16:27 +0200

    adding user-interface logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        boolean running = true;
+        while (running){
+        System.out.println("Enter a choice: encrypt, decrypt or exit. ");
+        String choice = bufferedReader.readLine();
+            if (choice.equals("encrypt")) {
+                encrypt cypherEncrypt = new encrypt();
+                System.out.println("Enter a text");
+                String inputReader = bufferedReader.readLine();
+                System.out.println("Enter a shift key");
+                String keyString = bufferedReader.readLine();
+                int keyInt = Integer.parseInt(keyString);
+                System.out.println(cypherEncrypt.caesarEncrypt(inputReader, keyInt));
+            } else if (choice.equals("decrypt")) {
+                Decrypt cypherDecrypt = new Decrypt();
+                System.out.println("Enter a text");
+                String inputReader = bufferedReader.readLine();
+                System.out.println("Enter a shift key");
+                String keyString = bufferedReader.readLine();
+                int keyInt = Integer.parseInt(keyString);
+                System.out.println(cypherDecrypt.caesarDecrypt(inputReader, keyInt));
+            } else if (choice.equals("exit")){
+                running = false;
+            }else {
+                System.out.println("unrecognized choice entry");
+            }
+        }
+    }
 }
