e60998a1e44416eb46c0a84754b83c9d49f58b51
Eliane-5 <irankundaeliane9@gmail.com>
2019-09-09 16:18:59 +0200

    adding Decryption business logic

--- src/main/java/Decrypt.java
+++ src/main/java/Decrypt.java
@@ -1,2 +1,29 @@
 public class Decrypt {
+    public static String caesarDecrypt(String textInput, int number){
+        String encryptText = "";
+        int length = textInput.length();
+        for (int i=0; i<length;i++){
+            char letter = textInput.charAt(i);
+            if (Character.isLetter(letter)){
+                if (Character.isLowerCase(letter)){
+                    char newLetter = (char)(letter-number);
+                    if(newLetter<'a'){
+                        encryptText += (char)(letter + (26-number));
+                    }else {
+                        encryptText += newLetter;
+                    }
+                }else if (Character.isUpperCase(letter)){
+                    char newLetter = (char)(letter-number);
+                    if(newLetter<'A'){
+                        encryptText += (char)(letter + (26-number));
+                    }else {
+                        encryptText += newLetter;
+                    }
+                }
+            }else{
+                encryptText += letter;
+            }
+        }
+        return encryptText;
+    }
 }
